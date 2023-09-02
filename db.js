7246ad1bfe2f1f03800d3cd5be0700528f4a6a31
Vikas Chauhan <vikas@rockstarchef.in>
2023-09-02 12:50:58 +0530

    setup environment variable

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg');
+
+const db = new Client({
+  user: process.env.POSTGRES_USERNAME,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,5 +1,14 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '../SECRET/.dev.env' });
+}
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const db = require('./db');
+
+const PORT = process.env.BACKEND_PORT;
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
@@ -10,6 +19,38 @@ app.get('/', (req, res) => {
   res.send('Hello docker worldd');
 });
 
-app.listen(process.env.BACKEND_PORT, () => {
-  console.log(`App listening on port ${process.env.BACKEND_PORT}`);
+app.get('/addUser', async (req, res) => {
+  try {
+    const { data } = req.body;
+    const query = "INSERT INTO user (name, email, password) VALUES ('vikas', 'vikas@gmail.com', 'vikas1')";
+    const values = [data];
+    await db.query(query, values);
+    res.status(201).json({ message: 'Data added' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+app.get('/check-connection', async (req, res) => {
+  try {
+    const query = 'SELECT NOW() AS current_time';
+    const result = await db.query(query);
+    const currentTime = result.rows[0].current_time;
+
+    res.status(200).json({ message: 'Database connected successfully', currentTime });
+    // pg.end()
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
+db.connect().then(() => {
+  db.query('SELECT NOW()', (err, res) => {
+    console.log(res.rows)
+    // pg.end()
+  });
 });
