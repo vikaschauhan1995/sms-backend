cbdc0a7210f549b6a49ef50de9b5a469140c8199
Vikas Chauhan <vikas@rockstarchef.in>
2023-09-23 14:25:49 +0530

    edit resonses of school apis

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -9,9 +9,9 @@ const createSchool = async (req, res) => {
       throw Error('All fields must be filled');
     }
     const school_id = uuidv4();
-    const query = `INSERT INTO school (${school.SCHOOL_ID}, ${school.SCHOOL_NAME}, ${school.MOBILE_NUMBER}, ${school.ADDRESS1}, ${school.ADDRESS2}, ${school.STATE}, ${school.PINCODE}, ${school.COUNTRY}, ${school.EXPIRATION_DATE}) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`;
-    await db.query(query, [school_id, school_name, mobile_number, address1, address2, state, pincode, country, expiration_date]);
-    res.status(200).json({ message: 'School added successfully' });
+    const query = `INSERT INTO school (${school.SCHOOL_ID}, ${school.SCHOOL_NAME}, ${school.MOBILE_NUMBER}, ${school.ADDRESS1}, ${school.ADDRESS2}, ${school.STATE}, ${school.PINCODE}, ${school.COUNTRY}, ${school.EXPIRATION_DATE}) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *`;
+    const newSchool = await db.query(query, [school_id, school_name, mobile_number, address1, address2, state, pincode, country, expiration_date]);
+    res.status(200).json(newSchool?.rows[0]);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -19,7 +19,7 @@ const createSchool = async (req, res) => {
 
 const getSchools = async (req, res) => {
   try {
-    const query = `SELECT * FROM school`;
+    const query = `SELECT * FROM school ORDER BY id DESC`;
     const schools = await db.query(query);
     res.status(200).json(schools.rows);
   } catch (err) {
@@ -43,6 +43,12 @@ const updateSchool = async (req, res) => {
   const { school_name, mobile_number, address1, address2, state, pincode, country, expiration_date } = req.body;
   const { school_id } = req.params;
   try {
+    if(!school_id){
+      throw Error('School ID is required');
+    }
+    if (!school_name || !mobile_number || !address1 || !address2 || !state || !pincode || !country) {
+      throw Error('All fields must be filled');
+    }
     const query = `UPDATE school SET 
       ${school.SCHOOL_NAME} = $1, 
       ${school.MOBILE_NUMBER} = $2, 
@@ -55,7 +61,7 @@ const updateSchool = async (req, res) => {
       WHERE ${school.SCHOOL_ID} = $9 RETURNING *`;
     console.log(query);
     const newSchool = await db.query(query, [school_name, mobile_number, address1, address2, state, pincode, country, expiration_date, school_id]);
-    res.status(200).json(newSchool.rows);
+    res.status(200).json(newSchool.rows?.[0]);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -66,7 +72,7 @@ const deleteSchool = async (req, res) => {
   try {
     const query = `DELETE FROM school WHERE ${school.SCHOOL_ID} = $1 RETURNING *`;
     const deletedSchool = await db.query(query, [school_id]);
-    res.status(200).json(deletedSchool.rows);
+    res.status(200).json(deletedSchool.rows?.[0]);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
